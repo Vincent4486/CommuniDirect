6794c7ce2fa4e3aaff22d11e8f3b2759e56f4a65
Vincent Yang <yang.vincent.h@icloud.com>
2026-02-21 08:35:10 +0800

    Add ClientLauncher and ServerLauncher classes; update build configuration

---
+++ communidirect-client/src/main/java/net/vincent/communidirect/client/ClientLauncher.java
@@ -0,0 +1,9 @@
+package net.vincent.communidirect.client;
+
+public class ClientLauncher {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+
+}
---
+++ communidirect-server/src/main/java/net/vincent/communidirect/server/ServerLauncher.java
@@ -0,0 +1,9 @@
+package net.vincent.communidirect.server;
+
+public class ServerLauncher {
+    
+    public static void main(String[] args) {
+        System.out.printf("Hello\n");
+    }
+
+}
